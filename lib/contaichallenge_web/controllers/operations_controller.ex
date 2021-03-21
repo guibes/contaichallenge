99ef24dcc59a1a991adb0f0892e149d5560cb10c
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-20 21:13:30 -0300

    Swagger Updated With Examples

--- lib/contaichallenge_web/controllers/operations_controller.ex
+++ lib/contaichallenge_web/controllers/operations_controller.ex
@@ -6,36 +6,6 @@ defmodule ContaichallengeWeb.OperationsController do
 
   action_fallback ContaichallengeWeb.FallbackController
 
-   #Create
-   swagger_path :create do
-    post "/api/operations/{operation}/{first_factor}/{second_factor}"
-    description("Create an operation in database")
-    summary "In this path we can create an "
-    produces "application/json"
-    parameters do
-      operation :path, :string, "Operations type: sum, sub, mul, div"
-      first_factor :path, :integer, "First factor to operate"
-      second_factor :path, :integer, "Second factor to operate"
-    end
-    response(:created, "Operation Created", Schema.ref(:Operation))
-  end
-
-  def swagger_definitions do
-    %{
-      Operation: swagger_schema do
-        title "Operation"
-        description "A operation in the database"
-        properties do
-          operation :string, "Operations type: sum, sub, mul, div"
-          first_factor :integer, "First factor to operate"
-          second_factor :integer, "Second factor to operate"
-          result :integer, "Result of the operation"
-          id :string, "ID of the operation"
-        end
-      end
-    }
-  end
-
   def create(conn, params) do
     with {:ok, %Operations{} = operation} <- Contaichallenge.create_operation(params) do
       conn
@@ -44,18 +14,7 @@ defmodule ContaichallengeWeb.OperationsController do
     end
   end
 
-  # Get All
-  swagger_path :getall do
-    get "/api/operations/"
-    description("Create an operation in database")
-    summary "List all operations in database"
-    produces "application/json"
-    parameters do
-      limit :query, :integer, "Limit number of rows", required: false
-      offset :query, :integer, "Offset the rows", required: false
-    end
-    response(200, "Get All Operations", Schema.ref(:Operation))
-  end
+
 
   def getall(conn, params) do
     with {:ok, operations} <- Contaichallenge.get_operations(params) do
@@ -64,5 +23,92 @@ defmodule ContaichallengeWeb.OperationsController do
       |> render("get_all.json", operations: operations)
     end
    end
+
+     #Create
+     swagger_path :create do
+      post "/api/operations/{operation}/{first_factor}/{second_factor}"
+      description("Create an operation in database")
+      summary "In this path we can create an "
+      produces "application/json"
+      parameters do
+        operation :path, :string, "Operations type: sum, sub, mul, div"
+        first_factor :path, :integer, "First factor to operate"
+        second_factor :path, :integer, "Second factor to operate"
+      end
+      response(:created, "Operation Created", Schema.ref(:Operation))
+    end
+
+    def swagger_definitions do
+      %{
+        Operation: swagger_schema do
+          title "Operation"
+          description "A operation in the database"
+          properties do
+            operation :string, "Operations type: sum, sub, mul, div"
+            first_factor :integer, "First factor to operate"
+            second_factor :integer, "Second factor to operate"
+            result :integer, "Result of the operation"
+            id :string, "ID of the operation"
+            message :string, "A message of response"
+          end
+          example %{
+            message: "Operation created",
+            operation: %{
+                first_factor: 1,
+                id: "99674d5e-5cdf-456c-9fb9-c4a2e6a2c0e8",
+                operation: "sum",
+                result: 3,
+                second_factor: 2
+            }
+        }
+        end,
+        Operations: swagger_schema do
+          title "Operations"
+          description "A operation in the database"
+          properties do
+            operation :string, "Operations type: sum, sub, mul, div"
+            first_factor :integer, "First factor to operate"
+            second_factor :integer, "Second factor to operate"
+            result :integer, "Result of the operation"
+            id :string, "ID of the operation"
+            message :string, "A message of response"
+            operationsCount :integer, "Number of operations listed"
+          end
+          example %{
+            message: "Operations listed",
+            operationsCount: 2,
+            operations: [
+              %{
+                first_factor: 1,
+                id: "99674d5e-5cdf-456c-9fb9-c4a2e6a2c0e8",
+                operation: "sum",
+                result: 3,
+                second_factor: 2
+              },
+              %{
+                first_factor: 50,
+                id: "af06c291-a2b5-404a-a5d0-efce2d04313c",
+                operation: "mul",
+                result: 1000,
+                second_factor: 20
+              },
+          ]
+        }
+        end
+      }
+    end
+
+    # Get All
+    swagger_path :getall do
+      get "/api/operations/"
+      description("Create an operation in database")
+      summary "List all operations in database"
+      produces "application/json"
+      parameters do
+        limit :query, :integer, "Limit number of rows", required: false
+        offset :query, :integer, "Offset the rows", required: false
+      end
+      response(200, "Get All Operations", Schema.ref(:Operations))
+    end
 
 end
