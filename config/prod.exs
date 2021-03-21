653d2d21562780a6fad23c2e0c868b413e80f713
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-21 10:02:33 -0300

    Docker and Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :contaichallenge, ContaichallengeWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "contaitest.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "contaichallenge.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
