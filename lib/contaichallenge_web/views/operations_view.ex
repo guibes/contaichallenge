48de1a15614436acca4189c710e0741c9cdfc923
Geovane Guibes <geovane.guibes@sigacred.com.br>
2021-03-21 10:48:29 -0300

    Formated operations_view

--- lib/contaichallenge_web/views/operations_view.ex
+++ lib/contaichallenge_web/views/operations_view.ex
@@ -13,16 +13,43 @@ defmodule ContaichallengeWeb.OperationsView do
   """
 
   def render("create.json", %{
-    operation: %Operations{
+      operation: %Operations{
+        id: id,
+        operation: operation,
+        first_factor: first_factor,
+        second_factor: second_factor,
+        result: result
+      }
+    }) do
+  %{
+    message: "Operation created",
+    operation: %{
+        id: id,
+        operation: operation,
+        first_factor: first_factor,
+        second_factor: second_factor,
+        result: result
+      }
+    }
+
+  end
+
+  @doc """
+    Function to render the get one response.
+    """
+
+  def render("get.json",
+  %{
+    operations: %Operations{
       id: id,
       operation: operation,
       first_factor: first_factor,
       second_factor: second_factor,
       result: result
     }
-  }) do
-%{
-  message: "Operation created",
+  }
+  ) do
+  %{
   operation: %{
       id: id,
       operation: operation,
@@ -32,37 +59,10 @@ defmodule ContaichallengeWeb.OperationsView do
     }
   }
 
-end
-
-@doc """
-  Function to render the get one response.
-  """
-
-def render("get.json",
-%{
-  operations: %Operations{
-    id: id,
-    operation: operation,
-    first_factor: first_factor,
-    second_factor: second_factor,
-    result: result
-  }
-}
-) do
-%{
-operation: %{
-    id: id,
-    operation: operation,
-    first_factor: first_factor,
-    second_factor: second_factor,
-    result: result
-  }
-}
-
-end
-@doc """
-  Function to render the get all response.
-  """
-def render("get_all.json", %{operations: operations}), do: %{operations: render_many(operations, OperationsView, "get.json"), operationsCount: length(operations), message: "Operations listed"}
+  end
+  @doc """
+    Function to render the get all response.
+    """
+  def render("get_all.json", %{operations: operations}), do: %{operations: render_many(operations, OperationsView, "get.json"), operationsCount: length(operations), message: "Operations listed"}
 
 end
